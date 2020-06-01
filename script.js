b0c87a3e351ef8e4e3ac3d81d4ac3ee95123bf76
Hetoreno <hectortamriel@gmail.com>
2020-06-01 04:33:03 -0700

    changes

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("styleText").onclick = function(){
+    document.getElementById("moreText").style.color = "red";
+    document.getElementById("moreText").style.fontSize= '50px';
+}
+
+document.getElementById('display').onclick = function(){
+    document.getElementById('new').style.display= 'none';
+}
